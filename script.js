cd72a8150b5c61e357bc4643b33fc47d8350539a
4lisson0 <113254957+4lisson0@users.noreply.github.com>
2022-10-27 11:01:26 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ let isIgnite = false
 function changeCard(event) {
     const card = event.currentTarget
     const backgroundImage = isIgnite
-    ? "url(/assets/bg-explorer.svg)"
-    : "url(/assets/bg-ignite-verde.svg)"
+    ? "url(assets/bg-explorer.svg)"
+    : "url(assets/bg-ignite-verde.svg)"
     isIgnite = !isIgnite 
     card.style.backgroundImage = backgroundImage
 }
